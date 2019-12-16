731845d30f36876e13a9a991cb942596b1c2ca43
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-12-16 12:52:06 +0200

    show how to work with functions in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
-function multiply(a: number, b: number): number {
-    return a * b;
+// Functions (optional and default parameters)
+
+function sum(a: number, b: number = 0): number {
+  return a + b;
 }
 
-const a = 5;
-const b = 2;
+sum(1);
 
-console.log(`${a} * ${b} = ${multiply(a, b)}`);
+type MyFunc = (a: number, b: number) => number;
+const sum2: MyFunc = (a, b) => a + b;
+
+// Unknown number of arguments
+
+function sumEverything(
+  arg1: string,
+  arg2: boolean,
+  ...numbers: number[]
+): number {
+  return numbers.reduce((result, num) => result + num, 0);
+}
+
+// Overloads
+
+function calcArea(width: number, height: number): number;
+function calcArea(length: number): number;
+function calcArea(...args: number[]): number {
+  if (args.length === 2) {
+    return args[0] * args[1];
+  }
+  return Math.pow(args[0], 2);
+}
